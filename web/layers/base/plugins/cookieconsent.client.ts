1e3989afd8cac13244274215ddc1b4a309db0ded
Damian <hi@damianrosellen.de>
2024-11-13 15:25:16 +0100

    update nuxt

--- web/layers/base/plugins/cookieconsent.client.ts
+++ web/layers/base/plugins/cookieconsent.client.ts
@@ -2,7 +2,7 @@
 // @ts-nocheck
 import 'vanilla-cookieconsent/dist/cookieconsent.css'
 import * as CookieConsent from 'vanilla-cookieconsent'
-import { useMainStore } from '~/stores/mainStore'
+import { useMainStore } from '~/app/stores/mainStore'
 
 export default defineNuxtPlugin(async () => {
 	const mainStore = useMainStore()
