2a87cc8f731855b12831b1726e244b80e35a1a0e
Damian <hi@damianrosellen.de>
2024-11-13 15:35:09 +0100

    update nuxt

--- web/layers/base/plugins/nuxtServerInit.server.ts
+++ web/layers/base/plugins/nuxtServerInit.server.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { useMainStore } from '~/app/stores/mainStore'
+import { useMainStore } from '~/stores/mainStore'
 
 export default defineNuxtPlugin(async ({ $pinia }) => {
 	const mainStore = useMainStore($pinia)
