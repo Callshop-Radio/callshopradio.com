62d8d6cd4fd93ec9f91a529e1b6e392845c0994b
Damian <hi@damianrosellen.de>
2024-12-09 19:25:54 +0100

    updates

--- web/server/api/sitemap.ts
+++ web/server/api/sitemap.ts
@@ -1,6 +1,6 @@
 import { SITEMAP_QUERY } from '~~/queries/sanity.queries'
-export default defineSitemapEventHandler(async () => {
+// export default defineSitemapEventHandler(async () => {
 	// const sanity = useSanity()
 	// const query = groq`${SITEMAP_QUERY}`
 	// return await sanity.fetch(query)
-})
+// })
