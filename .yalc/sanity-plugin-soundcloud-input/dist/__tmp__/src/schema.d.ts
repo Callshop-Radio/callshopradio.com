36ad16e27b69024772551b46bd50064214e0b8f1
Damian <hi@damianrosellen.de>
2025-08-25 19:08:08 +0200

    remove local soundcloud plugin installation

--- .yalc/sanity-plugin-soundcloud-input/dist/__tmp__/src/schema.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const soundcloud: {
-    type: "object";
-    name: "soundcloud";
-} & Omit<import("sanity").ObjectDefinition, "preview"> & {
-    preview?: import("sanity").PreviewConfig<Record<string, string>, Record<never, any>> | undefined;
-};
